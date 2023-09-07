ac136b670be7f39e475677365eaa8c62aeaf0b4f
Anandh Manickam <108410458+anandhmanickam@users.noreply.github.com>
2023-09-07 11:04:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,16 +72,21 @@ def on_startup():
 def create_hero(hero: HeroCreate):
     with Session(engine) as session:
         db_hero = Hero.from_orm(hero)
+        
+        # print(db_hero)                          #age=8 id=None name='Ad' secret_name='strng'
+        
         session.add(db_hero)
         session.commit()
         session.refresh(db_hero)
         return db_hero
 
-
 @app.get("/heroes/", response_model=List[HeroRead])
 def read_heroes():
     with Session(engine) as session:
         heroes = session.exec(select(Hero)).all()
+        
+        # print(heroes)                           #[Hero(id=2, age=8, name='Ad', secret_name='strng'), Hero(id=3, age=83, name='Ard', secret_name='Ad')] 
+       
         if not heroes:
             raise HTTPException(status_code=404, detail="No datas found")
         return heroes
@@ -90,6 +95,9 @@ def read_heroes():
 def read_hero(hero_id : int):
     with Session(engine) as session:
         hero = session.get(Hero, hero_id)
+        
+        # print(hero)                             #id=3 age=83 name='Ard' secret_name='Ad'
+        
         if not hero:
             raise HTTPException(status_code=404, detail="detail not found")
         return hero
@@ -98,11 +106,19 @@ def read_hero(hero_id : int):
 def update_hero(hero_id : int, hero : HeroUpdate):
     with Session(engine) as session:
         db_hero = session.get(Hero, hero_id)
+        
+        # print(db_hero)                          #id=3 age=83 name='Ard' secret_name='Ad'
+        
         if not db_hero:
             raise HTTPException(status_code=404, detail="Data Not Found")
         
         hero_data = hero.dict(exclude_unset=True)
+        
+        # print(hero_data)                        #{'name': 'Adj', 'secret_name': 'Adk', 'age': 86}
+        
         for key,value in hero_data.items():
+            # print(key)                          #name secret_name age
+            # print(value)                        #Adj Adk 86
             setattr (db_hero, key, value)
             
         session.add(db_hero)
