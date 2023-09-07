baccd454b26194121ea34ca7e63cd41a42201f7f
Anandh Manickam <108410458+anandhmanickam@users.noreply.github.com>
2023-09-07 10:45:02 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-# from sqlmodel import SQLModel, Field
-
-# from typing import Optional
-
-# class Hero(SQLModel,table=True):
-#     id : Optional[int] = Field(default=None, primary_key = True)
-#     name : str = Field(index = True)
-#     age : Optional[int]
