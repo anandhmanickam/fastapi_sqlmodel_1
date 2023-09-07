fd820aa1131bb7352ba4662055b9694c8947f12c
Anandh Manickam <108410458+anandhmanickam@users.noreply.github.com>
2023-09-07 12:56:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
@@ -46,16 +46,19 @@ url = "mysql://root@127.0.0.1:3306/modelfastapi"
 
 engine = create_engine(url, echo=True)
 
-
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
+    
+    
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 app = FastAPI()
 
 
 @app.on_event("startup")
-def on_startup():
+async def on_startup():
     create_db_and_tables()
 
 
@@ -69,11 +72,11 @@ def on_startup():
 
 
 @app.post("/heroes/", response_model=HeroRead)
-def create_hero(hero: HeroCreate):
+async def create_hero(*, session: Session = Depends(get_session), hero: HeroCreate):
     with Session(engine) as session:
         db_hero = Hero.from_orm(hero)
         
-        # print(db_hero)                          #age=8 id=None name='Ad' secret_name='strng'
+        # print(db_hero)                      #age=8 id=None name='Ad' secret_name='strng'
         
         session.add(db_hero)
         session.commit()
@@ -81,44 +84,44 @@ def create_hero(hero: HeroCreate):
         return db_hero
 
 @app.get("/heroes/", response_model=List[HeroRead])
-def read_heroes():
+async def read_heroes(*, session: Session = Depends(get_session)):
     with Session(engine) as session:
         heroes = session.exec(select(Hero)).all()
         
-        # print(heroes)                           #[Hero(id=2, age=8, name='Ad', secret_name='strng'), Hero(id=3, age=83, name='Ard', secret_name='Ad')] 
+        # print(heroes)                        #[Hero(id=2, age=8, name='Ad', secret_name='strng'), Hero(id=3, age=83, name='Ard', secret_name='Ad')] 
        
         if not heroes:
             raise HTTPException(status_code=404, detail="No datas found")
         return heroes
 
 @app.get("/heroes/{hero_id}", response_model=HeroRead)
-def read_hero(hero_id : int):
+async def read_hero(*, session: Session = Depends(get_session) , hero_id: int):
     with Session(engine) as session:
         hero = session.get(Hero, hero_id)
         
-        # print(hero)                             #id=3 age=83 name='Ard' secret_name='Ad'
+        # print(hero)                           #id=3 age=83 name='Ard' secret_name='Ad'
         
         if not hero:
             raise HTTPException(status_code=404, detail="detail not found")
         return hero
     
 @app.patch("/heroes/{hero_id}", response_model=HeroRead)
-def update_hero(hero_id : int, hero : HeroUpdate):
+async def update_hero(*, session: Session = Depends(get_session), hero_id: int, hero: HeroUpdate):
     with Session(engine) as session:
         db_hero = session.get(Hero, hero_id)
         
-        # print(db_hero)                          #id=3 age=83 name='Ard' secret_name='Ad'
+        # print(db_hero)                      #id=3 age=83 name='Ard' secret_name='Ad'
         
         if not db_hero:
             raise HTTPException(status_code=404, detail="Data Not Found")
         
         hero_data = hero.dict(exclude_unset=True)
         
-        # print(hero_data)                        #{'name': 'Adj', 'secret_name': 'Adk', 'age': 86}
+        # print(hero_data)                    #{'name': 'Adj', 'secret_name': 'Adk', 'age': 86}
         
         for key,value in hero_data.items():
-            # print(key)                          #name secret_name age
-            # print(value)                        #Adj Adk 86
+            # print(key)                      #name secret_name age
+            # print(value)                    #Adj Adk 86
             setattr (db_hero, key, value)
             
         session.add(db_hero)
@@ -128,7 +131,7 @@ def update_hero(hero_id : int, hero : HeroUpdate):
     
     
 @app.delete("/heroes/{hero_id}")
-def delete_hero(hero_id : int):
+async def delete_hero(*, session: Session = Depends(get_session), hero_id: int):
     with Session(engine) as session:
         hero = session.get(Hero, hero_id)
         if not hero:
@@ -136,4 +139,5 @@ def delete_hero(hero_id : int):
         session.delete(hero)
         session.commit()
         return {"ok": True}
-        
+    
+    
